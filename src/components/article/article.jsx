649ecd5e4b8d560b7d7e15e5ca14a2efb398adbd
Emanuel Kluge <emanuel.kluge@jimdo.com>
2017-03-12 17:52:28 +0100

    Add link to article & copy-URL button

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -7,8 +7,24 @@
 
 import React, { PropTypes } from 'react';
 import { Link, Helpers } from 'react-scroll';
+import classnames from 'classnames';
+import mdlBtn from 'material-design-lite/src/button/_button.scss';
 import TimeAgo from '../time-ago/time-ago';
+import CopyButton, { hasClipboardSupport } from '../copy-button/copy-button';
 import styles from './article.sass';
+
+const btnClass = classnames(styles.button, mdlBtn['mdl-button']);
+
+const copyToClipBoardBtn = (url) => {
+    if (hasClipboardSupport()) {
+        return (<CopyButton
+            className={btnClass}
+            caption="Copy article-URL"
+            data={url}
+        />);
+    }
+    return null;
+};
 
 const Article = ({ name, id, url, color, title, intro, content, created_at, onSetActive }) =>
     (<article className={styles.article}>
@@ -28,6 +44,17 @@ const Article = ({ name, id, url, color, title, intro, content, created_at, onSe
             <em>{intro}</em>
         </p>
         <div dangerouslySetInnerHTML={{ __html: content }} />
+        <footer className={styles.bottom}>
+            <a
+                href={url}
+                className={btnClass}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                View original article
+            </a>
+            {copyToClipBoardBtn(url)}
+        </footer>
     </article>);
 
 Article.propTypes = {
