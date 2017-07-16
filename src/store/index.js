1ced8b1eac53102196d2a8382bae95589610da5b
Emanuel Kluge <kraemerseele@gmx.de>
2017-07-16 12:13:07 +0200

    chore: disable redux-logger in prod env

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,23 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
-import { createLogger } from 'redux-logger';
 import reducers from './reducers';
 import rootEpic from './epics/';
+import { SET_IS_APPENDING_ARTICLE } from './actions';
 
-const epicMiddleware = createEpicMiddleware(rootEpic);
-const logger = createLogger({ collapsed: true });
-const store = createStore(reducers, applyMiddleware(epicMiddleware, logger));
+const middlewares = [createEpicMiddleware(rootEpic)];
+
+if (__DEV__) {
+    // eslint-disable-next-line global-require
+    const { createLogger } = require('redux-logger');
+    const loggerOptions = {
+        collapsed: true,
+        predicate: (getState, action) => action.type !== SET_IS_APPENDING_ARTICLE,
+    };
+    middlewares.push(createLogger(loggerOptions));
+}
+
+const store = createStore(reducers, applyMiddleware(...middlewares));
 
 export default function getStore() {
     return store;
