df36538b69210e611a3e1f5df44fbed12a9488a5
Emanuel Kluge <kraemerseele@gmx.de>
2019-11-02 13:14:22 +0100

    feat: deprecate app & add shot-down notice (#18)

--- src/store/epics/index.js
+++
@@ -1,5 +0,0 @@
-import { combineEpics } from 'redux-observable';
-import indexEpic from './index-view';
-import detailEpic from './detail-view';
-
-export default combineEpics(indexEpic, detailEpic);
